9280a463c4304834274d095d91f80e42c57ebeab
Harry Merritt <harry@merritt.es>
2020-08-17 00:30:26 +0100

    Complete Merging Two Packages

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,27 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    #
+    result = None
+    limit_diff = {}
 
-    return None
+    # Populate [weight] = limit - weight
+    for index, weight in enumerate(weights):
+        if weight not in limit_diff:
+            limit_diff[weight] = (index, limit - weight)
+
+    for key, value in limit_diff.items():
+        diff = value[1]
+        if diff in weights:
+            diff_index = limit_diff[diff][0]
+            if diff_index == value[0]:
+                diff_index = weights[diff_index + 1:].index(diff) + diff_index + 1
+                if diff_index == None: continue
+            result = (diff_index, value[0])
+            break
+
+    return result
+
+weights_2 = [4, 4]
+answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
+print(answer_2)
